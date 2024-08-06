381c230d7a6ac2e7bebe5b7b731348a3fa9700dc
Vineet <vineetprashant1@gmail.com>
2024-08-07 00:22:47 +0530

    ai integration and backend

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const zod_1 = __importDefault(require("zod"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+const PORT = process.env.PORT || 3000;
+const generative_ai_1 = require("@google/generative-ai");
+const genAI = new generative_ai_1.GoogleGenerativeAI(process.env.API_KEY || "");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+app.post("/imageToText", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const body = req.body;
+    const schema = zod_1.default.object({
+        blob: zod_1.default.any(),
+    });
+    const validation = schema.safeParse(body);
+    // console.log(body, validation);
+    if (!validation.success) {
+        res.status(400).json({ success: false, message: "Invalid body" });
+        return;
+    }
+    const imagePart = {
+        inlineData: {
+            data: (_a = validation.data) === null || _a === void 0 ? void 0 : _a.blob,
+            mimeType: "image/png",
+        },
+    };
+    let result;
+    const prompt = "This is an image generated in a software similar to ms-paint. Try to describe it the best you can in the least number of words required";
+    try {
+        result = yield model.generateContent([prompt, imagePart]);
+        res.json({ success: true, message: result.response.text() });
+    }
+    catch (err) {
+        console.error("Error :", err);
+        res.status(500).json({
+            success: false,
+            message: "Failed to generate text",
+        });
+    }
+    // console.log(result.response.text());
+}));
+app.listen(PORT, () => {
+    console.log("Server listening on port", PORT);
+});
--- index.js
+++ index.js
@@ -1,48 +1,58 @@
+const URL = "http://localhost:3002";
 const canvas = document.getElementById("canvas");
 const colorInput = document.getElementById("color-input");
 const colorDisplay = document.getElementById("stroke-color");
 const widthInput = document.getElementById("width-input");
-const widthDisplay = document.getElementById("stroke-width");
+const widthDisplay = document.getElementById("width-input-div");
 const saveButton = document.getElementById("save-button");
 const clearButton = document.getElementById("clear-button");
 const fillButton = document.getElementById("fill-button");
+const guessDiv = document.getElementById("ai-guess");
 const ctx = canvas.getContext("2d");
+
 let boundings = canvas.getBoundingClientRect();
+let intervalId;
 canvas.addEventListener("mousedown", (e) => handleMouseDown(e));
-canvas.addEventListener("mouseup", (e) => handleMouseUp(e));
+window.addEventListener("mouseup", (e) => handleMouseUp(e));
 canvas.addEventListener("mousemove", (e) => handleDrawing(e));
 colorInput.addEventListener("change", (e) => changeColor(e));
-widthInput.addEventListener("change", (e) => changeWidth(e));
+widthInput.addEventListener("input", (e) => changeWidth(e));
 saveButton.addEventListener("click", saveImage);
 clearButton.addEventListener("click", clearCanvas);
 fillButton.addEventListener("click", fillCanvas);
-ctx.strokeStyle = colorInput.value;
 
+ctx.strokeStyle = colorInput.value;
+let isDrawing = false;
 setInterval(() => (boundings = canvas.getBoundingClientRect()), 1000);
 function handleMouseDown(event) {
+    clearTimeout(intervalId);
     let { mouseX, mouseY } = getMouseCoords(event);
-    console.log("down");
     ctx.beginPath();
     ctx.moveTo(mouseX, mouseY);
     isDrawing = true;
 }
 function handleMouseUp() {
-    console.log("up");
+    if (isDrawing) {
+        intervalId = setTimeout(async () => {
+            guessDiv.innerHTML = await guessWhatsThat();
+        }, 1000);
+    }
+
     ctx.closePath();
     isDrawing = false;
 }
 function handleDrawing(event) {
     if (isDrawing) {
         let { mouseX, mouseY } = getMouseCoords(event);
-        console.log("moving");
+
         ctx.lineTo(mouseX, mouseY);
         ctx.stroke();
     }
 }
 
 function getMouseCoords(event) {
-    mouseX = event.clientX - boundings.left;
-    mouseY = event.clientY - boundings.top;
+    let mouseX = event.clientX - boundings.left;
+    let mouseY = event.clientY - boundings.top;
     return { mouseX, mouseY };
 }
 
@@ -52,7 +62,7 @@ function changeColor(event) {
 }
 function changeWidth(event) {
     ctx.lineWidth = event.target.value;
-    widthDisplay.innerHTML = event.target.value;
+    widthDisplay.title = `Thickness: ${event.target.value}`;
 }
 
 function saveImage() {
@@ -61,6 +71,22 @@ function saveImage() {
     a.href = canvasDataURL;
     a.download = "MyPainting";
     a.click();
+}
+
+async function guessWhatsThat() {
+    const canvasDataURL = canvas.toDataURL();
+    const res = await fetch(`${URL}/imageToText`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ blob: `${canvasDataURL.slice(22)}` }),
+    });
+    const data = await res.json();
+    if (data.success) {
+        return data.message;
+    }
+    return "I dont know what that is...";
 }
 
 function clearCanvas() {
